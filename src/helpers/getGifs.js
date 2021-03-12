5544c98caae0d830d60b0c1857275f06b34a2a98
Sebastian Vega <sebastian.vega.23@gmail.com>
2021-03-12 17:24:04 -0300

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async ( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=HKS4K7UU9sKjCBfrEsHFOsQZvh7W0b8z&q=${ encodeURI(category) }&limit=10`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+
+}
